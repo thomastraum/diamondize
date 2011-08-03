d14f16c7865245ae7235167278debdfa3b93a14b
Thomas Eberwein <tommi@thomastraum.com>
2011-08-03 14:22:14 +0100

    angler fish, moved pics in pics

--- diamondize draw 1/src/testApp.cpp
+++ diamondize draw 1/src/testApp.cpp
@@ -6,7 +6,7 @@ void testApp::setup(){
 
     ofSetFrameRate(10);
 
-    img.loadImage( "pics/cat.gif" ); //"Tiger Face.jpg" );
+    img.loadImage( "pics/angler-fish1.jpg" ); //"Tiger Face.jpg" );
     
     show_triangulation  = false;
     show_image          = true;
